3a5a1938b055f2f7d1a081c804753c03fea051e6
rafamrrobot <rafamrrobot@gmail.com>
2023-03-15 20:46:18 -0300

    Updates

---
+++ precache-manifest.4e4c85e0fc013725f21371b7c0a6d7bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c51a76b9c9f5a1b7dcd7",
+    "url": "/Controle-de-financas/static/js/main.c51a76b9.chunk.js"
+  },
+  {
+    "revision": "e46b4b71e66044c897e2",
+    "url": "/Controle-de-financas/static/js/1.e46b4b71.chunk.js"
+  },
+  {
+    "revision": "88ee220152a7c72975ed",
+    "url": "/Controle-de-financas/static/js/runtime~main.88ee2201.js"
+  },
+  {
+    "revision": "9dcc63ece66bf45f6993cd3f4747f837",
+    "url": "/Controle-de-financas/index.html"
+  }
+];
